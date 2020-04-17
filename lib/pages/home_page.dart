72df4a6be63d60d2fa533fb0e84fdbaf496d79f6
enrique <enrique_11_3@hotmail.com>
2020-04-16 20:59:43 -0400

    proyecto

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+import 'package:speech_recognition/speech_recognition.dart';
+import 'package:speechtotext/utils/widget_utils.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  SpeechRecognition _speechRecognition;
+  bool _isAvailable = false;
+  bool _isListening = false;
+  final _speechController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+  String resultText = "";
+
+  @override
+  void initState() {
+    super.initState();
+    initSpeechRecognizer();
+  }
+
+  void initSpeechRecognizer(){
+    _speechRecognition = SpeechRecognition();
+
+    _speechRecognition.setAvailabilityHandler(
+        (bool result) => setState(() => _isAvailable = result)
+    );
+
+    _speechRecognition.setRecognitionStartedHandler(
+        () => setState(()=> _isListening = true)
+    );
+
+    _speechRecognition.setRecognitionResultHandler(
+        (String speech) => setState(() => resultText = speech)
+    );
+
+    _speechRecognition.setRecognitionCompleteHandler(
+            () => setState(()=> _isListening = false)
+    );
+
+    _speechRecognition.activate().then(
+        (result) => setState (() => _isAvailable = result)
+    );
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+           // _createButtons(),
+            SizedBox(height: 20.0,),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.8,
+              height: MediaQuery.of(context).size.width * 0.3,
+              child: Text(resultText,style: TextStyle(fontSize: 16),),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(6.0),
+                border: Border.all(),
+              ),
+            ),
+            SizedBox(height: 20.0,width: 20.0,),
+            crearInputMessage("mensaje","mensaje",_speechController,_speaking)
+
+          ],
+        ),
+      ),
+
+    );
+  }
+  _speaking(){
+  if(_isAvailable && !_isListening)
+      _speechRecognition.listen(locale: "es_ES").then(
+     (result) => print('$result')
+      );
+  else
+    _speechRecognition.stop().then(
+    (result) => setState(() => _isListening = result),
+      );
+  setState(() {
+    resultText = _speechController.text;
+  });
+
+}
+//  Widget _createButtons(){
+//    return Row(
+//      mainAxisAlignment: MainAxisAlignment.center,
+//      children: <Widget>[
+//
+//
+//        FloatingActionButton(
+//            child: Icon(Icons.mic),
+//            onPressed: _speaking,
+//          ),
+//
+//        FloatingActionButton(
+//          mini: true,
+//          backgroundColor: Colors.red,
+//          child: Icon(Icons.stop),
+//          onPressed: (){
+//            if (_isListening)
+//              _speechRecognition.cancel().then(
+//                    (result) => setState(() {
+//                  _isListening = result;
+//                  resultText = "";
+//                }),
+//              );
+//          },
+//        ),
+//
+//       ],
+//     );
+//  }
+}
---
+++ lib/utils/widget_utils.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:speechtotext/utils/utils.dart' as utils;
+
+  Widget crearInputString(String hintT,String labelT,TextEditingController controller) {
+
+    return TextFormField(
+
+      textCapitalization: TextCapitalization.words,
+      controller: controller,
+      decoration: InputDecoration(
+          border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20.0)
+          ),
+          hintText: hintT,
+          labelText: labelT
+      ),
+    );
+  }
+
+  // ignore: non_constant_identifier_names
+  Widget crearInputNumeric(String HintT,String LabelT, double Valor,TextEditingController controller) {
+
+    return TextFormField(
+      controller: controller,
+      textCapitalization: TextCapitalization.words,
+      keyboardType: TextInputType.number,
+      decoration: InputDecoration(
+          border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20.0)
+          ),
+          hintText: HintT,
+          labelText: LabelT
+      ),
+      onSaved: (value)=> Valor = double.parse(value),
+      validator: (value) {
+        if(utils.isNumeric(value)) {
+          return null;
+        } else {
+          return 'Ingrese solo numeros';
+        }
+      },
+    );
+  }
+
+Widget crearInputMessage(String hintT,String labelT,TextEditingController controller,Function func) {
+
+  return  TextFormField(
+    textCapitalization: TextCapitalization.words,
+    controller: controller,
+    decoration: InputDecoration(
+      border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(15.0)
+      ),
+      hintText: hintT,
+      labelText: labelT,
+      suffixIcon:Ink(
+        decoration: const ShapeDecoration(
+          color: Colors.lightBlue,
+          shape: CircleBorder(),
+        ),
+        child: IconButton(
+          icon: Icon(Icons.mic),
+          color: Colors.white,
+          onPressed: func,
+        ),
+      ),
+    ),
+
+  );
+
+
+}
+
